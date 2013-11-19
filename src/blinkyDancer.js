65bc63f07597477297b0801acc642ee58c0631b4
Mehul Patel <mehul8@gmail.com>
2013-11-19 00:21:58 -0800

    finally passed tests. blinky dancers work

--- src/blinkyDancer.js
+++ src/blinkyDancer.js
@@ -1,7 +1,8 @@
 var BlinkyDancer = function(top, left, timeBetweenSteps){
   Dancer.call(this, top, left, timeBetweenSteps);
-  this.oldstep = this.step;
-  //console.log(this);
+  // this.oldStep = Dancer.prototype.step;
+  // this.timeBetweenSteps = timeBetweenSteps;
+  // this.step;
 
   // we plan to overwrite the step function below, but we still want the superclass step behavior to work,
   // so we must keep a copy of the old version of this function
@@ -10,8 +11,6 @@ var BlinkyDancer = function(top, left, timeBetweenSteps){
 BlinkyDancer.prototype = Object.create(Dancer.prototype);
 BlinkyDancer.prototype.constructor = BlinkyDancer;
 BlinkyDancer.prototype.step = function(){
-  var that = this;
-  //console.log(that);
-  Dancer.prototype.step();
-  //that.$node.toggle();
+  Dancer.prototype.step.call(this);
+  this.$node.toggle();
 };
--- src/dancer.js
+++ src/dancer.js
@@ -1,14 +1,16 @@
 // Creates and returns a new dancer object that can step
 var Dancer = function(top, left, timeBetweenSteps){
 
-  this.timeBetweenSteps = timeBetweenSteps;
+
   // use jQuery to create an HTML <span> tag
-  this.step();
+
   this.$node = $('<span class="dancer"></span>');
+  this.timeBetweenSteps = timeBetweenSteps;
+  this.step();
 
   // now that we have defined the this object, we can start setting up important parts of it by calling the methods we wrote
   // this one sets the position to some random default point within the body
-  //this.setPosition(top, left);
+  this.setPosition(top, left);
 };
 
 Dancer.prototype.setPosition = function (top, left){
@@ -23,5 +25,6 @@ Dancer.prototype.setPosition = function (top, left){
 Dancer.prototype.step = function (){
   var that = this;
   //debugger;
-  setTimeout(function(){that.step();}, that.timeBetweenSteps);
+  setTimeout(function(){ that.step(); }, that.timeBetweenSteps);
 };
+
